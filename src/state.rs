f0b4d2faadbf64f4cac4edd0817897624a3d7313
karikari-sembey <213266724+karikari-sembey@users.noreply.github.com>
2026-02-07 13:49:44 +0900

    chore: 使用予定のない変数を削除

--- src/state.rs
+++ src/state.rs
@@ -3,6 +3,4 @@ pub struct VRChatState {
     pub instance_users: Vec<String>,
     pub instance_owner: String,
     pub instance_permission: String,
-    pub world_id: String,
-    pub world_name: String,
 }
